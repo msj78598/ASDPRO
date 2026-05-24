09e3d77a8735eaae368249fd5843215727aab55c
msj78598 <msj78598@users.noreply.github.com>
2026-05-24 14:36:41 +0300

    Make Streamlit launcher portable

--- run_app.ps1
+++ run_app.ps1
@@ -1,2 +1,2 @@
 Set-Location -Path $PSScriptRoot
-& "C:\Users\78598\AppData\Local\Programs\Python\Python310\python.exe" -m streamlit run app.py --server.port 8501 --server.headless true *> streamlit.run.log
+python -m streamlit run app.py --server.port 8501 --server.headless true
